a108aafca9099234a8556ba6e94d91b3f7eeeb1d
aa <aa@DESKTOP-SHMIR38>
2023-08-08 17:03:13 +0530

    Third Commit

---
+++ Myjava/src/com/mywork/Variables.java
@@ -0,0 +1,17 @@
+package com.mywork;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2;
+		
+		System.out.println(a);
+		
+
+		
+	}
+
+}
+
